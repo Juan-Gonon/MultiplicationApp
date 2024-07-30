7790486dd85bb84b374fc499c910ebf9203aed45
Juan Gonon <juanfegonon27@gmail.com>
2024-07-30 10:29:52 -0600

    test save-file

---
+++ test/domain/use-case/save-file.use-case.test.ts
@@ -0,0 +1,29 @@
+import { SaveFile } from '../../../src/domain/use-cases/save-filte.use-case'
+import fs, { ftruncate }  from 'fs'
+
+describe('SaveFileUseCase', () => {
+
+    it('should save file with default values', () => {
+
+        const options = {
+            fileContent: 'test content'
+        }
+        const filePath= 'outputs/table.txt'
+
+        const saveFile = new SaveFile()
+
+        const result = saveFile.execute(options)
+        const fileExist = fs.existsSync(filePath)
+        const fileContent = fs.readFileSync(filePath, { encoding: 'utf-8' })
+
+        expect(result).toBeTruthy()
+        expect(fileExist).toBeTruthy()
+        expect(fileContent).toBe(options.fileContent)
+
+
+    
+
+
+    })
+
+})
